b4d46b18eb38f2364c15d52aa4a6c30b6cbaa549
ANIL <ANIL.KINIKAR@GMAIL.COM>
2012-06-24 02:17:33 +0530

    course model
    course model commit

---
+++ models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class CourseCategory(models.Model):
+    name = models.CharField(max_length=255)
+    placement = models.IntegerField()
+
+    
+class CourseGroup(models.Model):
+    title = models.CharField(max_length=255)
+    sanitizedTitle = models.CharField(max_length=255)
+      
+
+class Course(models.Model):
+    description = models.TextField()
+    title = models.CharField(max_length=255)
+    sanitizedTitle = models.CharField(max_length=255)
+    category = models.ForeignKey(CourseCategory)
+    group = models.ForeignKey(CourseGroup)
+    
